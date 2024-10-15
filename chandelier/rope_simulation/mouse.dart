6ad0bcfd91d10d10370606b65f01784a47ce7adc
Potter_Parker <54338784+OmarElsiry@users.noreply.github.com>
2024-10-15 20:16:55 +0300

    Create mouse.dart

---
+++ chandelier/rope_simulation/mouse.dart
@@ -0,0 +1,10 @@
+
+// rope_simulation/mouse.dart
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+class Mouse {
+  vector.Vector2 pos;
+  double radius;
+
+  Mouse(this.pos) : radius = 40;
+}
